6bd594fc16b9306aae820c9670375600bcbda817
Samandar <86640731+samandaru@users.noreply.github.com>
2021-07-15 19:54:17 +0900

    forloop

---
+++ forloop.js
@@ -0,0 +1,22 @@
+// juft toq sonlar
+
+for(let i=1;i<=10;i++){
+    if (i%2 === 0) {
+        console.log(i+"juft son") 
+    }else{
+        console.log(i+"toq son")
+    }
+}
+
+// 10,9.......1,0
+for (let i = 10; i >=0 ; i--) {
+    console.log(i);
+    
+}
+
+// 5 & 3ga bulinadigan sonlar
+for(let i=1; i<=100; i++){
+    if (i*15<100) {
+        console.log(i*15);
+    }
+}
